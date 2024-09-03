8fe4fa9f317f09cd096b609b24a526602eed67d0
brkckr20 <burak2043@gmail.com>
2024-09-03 18:17:06 +0300

    update whatido started

---
+++ src/components/UpdateWhatIDo.vue
@@ -0,0 +1,48 @@
+
+
+<template lang="">
+    <div class="relative z-10 font-poppins" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+
+  <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
+
+  <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+    <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+
+      <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+        <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+          <div class="sm:flex flex-col sm:items-start ">
+            <label class="block">İkon</label>
+            <input v-model="item.ikon" class="border w-full p-1" />
+            <label class="block">Alt Başlık</label>
+            <input v-model="item.altBaslik" class="border w-full p-1" />
+            <label class="block">İçerik</label>
+            <textarea v-model="item.icerik" class="border w-full p-1 h-28"></textarea>
+        </div>
+        </div>
+        <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+          <button type="button" @click="handleUpdateItem" class="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 sm:ml-3 sm:w-auto">Güncelle</button>
+          <button type="button" @click="$emit('close')" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">İptal</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+</template>
+
+
+<script setup>
+import { defineProps, ref  } from 'vue';
+const props = defineProps(['item']);
+const item = props.item; 
+
+const handleUpdateItem = async() => {
+    console.log("Güncelleme işlemi yapılacaktır.");
+}
+
+</script>
+
+
+<style lang="">
+    
+</style>
--- src/pages/Admin/WhatIDo.vue
+++ src/pages/Admin/WhatIDo.vue
@@ -3,62 +3,84 @@
         <AdminHeader></AdminHeader>
         <div class="font-poppins max-w-7xl mx-auto">
             <form @submit.prevent="handleUpdate">
-                <label class="block mb-1">Başlık</label>
-                <input v-model="formValues.baslik" class="border w-full p-1"/>
                 <label class="block mb-1">İkon</label>
                 <input v-model="formValues.ikon" class="border w-full p-1"/>
                 <label class="block mb-1">Alt Başlık</label>
                 <input v-model="formValues.altBaslik" class="border w-full p-1"/>
                 <label class="block mb-1">İçerik</label>
                 <input v-model="formValues.icerik" class="border w-full p-1"/>
-                <button class="bg-violet-500 h-10 text-white px-3 mt-2 hover:bg-violet-700 duration-200 ease-in-out hover:rounded-lg">Kaydet</button>   
-
+                <button class="bg-violet-500 h-10 text-white px-3 mt-2 hover:bg-violet-700 duration-200 ease-in-out hover:rounded-lg">Kaydet</button>
             </form>
-
+            <table class="table-auto w-full mt-2">
+                <thead class="bg-gray-500 text-white">
+                    <tr class="border">
+                        <th>İkon</th>
+                        <th>Alt Başlık</th>
+                        <th>İçerik</th>
+                        <th>İşlem</th>
+                    </tr>
+                </thead>
+                <tbody class="text-center border">
+                    <tr v-for="item in items">
+                        <td class="border"><i :class="item.ikon" class="text-3xl text-main"></i></td>
+                        <td class="border">{{ item.altBaslik }}</td>
+                        <td class="border">{{ item.icerik }}</td>
+                        <td class="border">
+                            <button class="mr-2" @click="updateItem(item)">
+                                <i class="fa-regular fa-pen-to-square"></i>
+                            </button>
+                            <button @click="deleteItem(item.id)">
+                                <i class="fa-solid fa-trash-can text-red-600"></i> 
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
         <notifications position="top right"/>
+        <UpdateWhatIDo v-if="openModal" @close="openModal = false" :item="selectedItem" />
     </div>
 </template>
 <script setup>
     import AdminHeader from './AdminHeader.vue';
-    import { db, collection, addDoc, getDocs } from '../../../firebase';
+    import UpdateWhatIDo from '../../components/UpdateWhatIDo.vue'
+    import { db, collection, addDoc, getDocs ,deleteDoc} from '../../../firebase';
     import { doc, updateDoc } from 'firebase/firestore';
     import {ref,onMounted} from 'vue';
-    import {success} from '../../utils'
-
+    import { success } from '../../utils'
+    const collectionRef = collection(db, 'neyaparim');
     const formValues = ref({
-        baslik : 'asd',
-        ikon : 'asdasdasd',
-        altBaslik : 'asdasdasd11',
-        icerik : 'asdaasdsdas1111d11',
+        ikon : '',
+        altBaslik : '',
+        icerik : '',
         id : '',
     });
+    const openModal = ref(false);
     const items = ref([]);
     const fetchData = async () => {
-        const querySnapshot = await getDocs(collection(db, 'hakkimda'));
+        const querySnapshot = await getDocs(collection(db, 'neyaparim'));
         items.value = querySnapshot.docs.map(doc => ({
         id: doc.id,
         ...doc.data()
     }));
-    if (items.value.length > 0) {
-        formValues.value.baslik = items.value[0].baslik || '';
-        formValues.value.ikon = items.value[0].ikon || '';
-        formValues.value.altBaslik = items.value[0].altBaslik || '';
-        formValues.value.icerik = items.value[0].icerik || '';
-        formValues.value.id = items.value[0].id || '';
-        }
+    // if (items.value.length > 0) {
+    //     formValues.value.ikon = items.value[0].ikon || '';
+    //     formValues.value.altBaslik = items.value[1].altBaslik || '';
+    //     formValues.value.icerik = items.value[0].icerik || '';
+    //     formValues.value.id = items.value[0].id || '';
+    //     }
         
     }
     onMounted(fetchData)
 
     const handleUpdate = async () => {
         await addDoc(collection(db,'neyaparim'), {
-            baslik : formValues.value.baslik,
             ikon : formValues.value.ikon,
             altBaslik : formValues.value.altBaslik,
             icerik : formValues.value.icerik,
         });
-        success("success","Kayıt")
+        success("success","Kayıt");
+        fetchData();
                 
     //     try {
     //   if (formValues.value.id) {
@@ -74,6 +96,23 @@
     //   alert(error)
     // }
     
+    }
+    
+    const deleteItem =  async (id) =>{
+        const docRef = doc(collectionRef, id);
+        await deleteDoc(docRef);
+        fetchData();
+        success("success", "Silme");
+    }
+    const selectedItem = ref({
+        id : "",
+        altBaslik : "",
+        icerik : "",
+        ikon : ""
+    });
+    const updateItem =  async (item) =>{
+        openModal.value = true;
+        selectedItem.value = item;
     }
 </script>
 <style lang="">
